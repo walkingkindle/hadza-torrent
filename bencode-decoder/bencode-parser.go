3685e8a30252b9d31dd970d85f290dcef0304435
walkingkindle <aki.hadza@gmail.com>
2026-06-13 20:09:23 +0200

    getting somewhere with bencode parser

--- bencode-decoder/bencode-parser.go
+++ bencode-decoder/bencode-parser.go
@@ -1,79 +1,100 @@
 package bencodeparser
 
 import (
-	"bytes"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 )
 
-var torrentBytes = []byte{}
-
 func Decode(location string) ([]byte, error) {
 	bytes, err := os.ReadFile(location)
 	if err != nil {
-		return nil, fmt.Errorf("could not open the file %q: %w", location, err)
+		return nil, fmt.Errorf("could nt open the file %q: %w", location, err)
 	}
 
-	torrentBytes = bytes
-
 	position := 0
-	for i := 0; i < len(torrentBytes); {
+	for i := 0; i < len(bytes); {
 		switch bytes[position] {
 		case 'd':
-			return parsedict(position)
+			return parsedict(position, bytes)
 		}
 	}
 
 	return bytes, nil
 }
 
-func parsedict(position int) ([]byte, error) {
-	if torrentBytes[position] != 'd' {
-		return nil, fmt.Errorf("not a dictionary at position %d", position)
+func parsedict(position int, bytesArr []byte) ([]byte, error) {
+	if bytesArr[position] != 'd' {
+		return nil, fmt.Errorf("not a dctionary at position %d", position)
 	}
+	position++
 
-	lengthStartPosition := position + 1
+	m := make(map[string]string)
+	for bytesArr[position] != 'e' {
+		key, pos, keyErr := parseString(position, bytesArr)
+		fmt.Printf("\n key is %s \n", key)
+		fmt.Printf("pos is %d", pos)
+		position = pos
 
-	length, absoluteColonIdx, err := getLength(lengthStartPosition)
-	if err != nil {
-		fmt.Printf("formatting error, %s", err)
+		if keyErr != nil {
+			return bytesArr, keyErr
+		}
+
+		value, pos, err := parseString(position, bytesArr)
+
+		if err != nil {
+			return bytesArr, err
+		}
+
+		fmt.Printf("\n value is %s \n", value)
+		fmt.Printf("pos is %d", pos)
+		position = pos
+
+		m[key] = value
+
 	}
-
-	key, keyEndPosition := extractKey(absoluteColonIdx, length)
-
-	fmt.Printf("key and position are %s, %d", key, torrentBytes[keyEndPosition])
-
-	newLength, _, _ := getLength(keyEndPosition)
-
-	fmt.Printf("%d \n", newLength)
-
-	key2, _ := extractKey(newLength, newLength)
-
-	fmt.Printf("%s \n", key2)
 
 	return nil, nil
 }
 
-func extractKey(absoluteColonIdx int, length int) (string, int) {
-	stringDataStart := absoluteColonIdx + 1
-	stringDataEnd := stringDataStart + length
-	stringValue := torrentBytes[stringDataStart:stringDataEnd]
-	return string(stringValue), stringDataEnd
+func parseString(position int, bytes []byte) (string, int, error) {
+	length, newPosition, err := getLength(position, bytes)
+	if err != nil {
+		return "", -1, err
+	}
+
+	position = newPosition
+
+	value, newPosition := getStringValue(position, length, bytes)
+
+	return value, newPosition, nil
 }
 
-func getLength(lengthStartPosition int) (int, int, error) {
-	relativevColonIndx := bytes.IndexByte(torrentBytes[lengthStartPosition:], ':')
-	if relativevColonIndx == -1 {
-		return 0, 0, fmt.Errorf("colon not found")
-	}
-	absoluteColonIdx := lengthStartPosition + relativevColonIndx
+func getStringValue(position int, length int, byteArr []byte) (string, int) {
+	value := byteArr[position:(position + length)]
 
-	stringLengthBytes := torrentBytes[lengthStartPosition:absoluteColonIdx]
+	return string(value), (position + length)
+}
 
-	length, err := strconv.Atoi(string(stringLengthBytes))
+func getLength(lengthStartPosition int, bytesArr []byte) (int, int, error) {
+	lengthEndPosition := indexOf(bytesArr[lengthStartPosition:], ':')
+	absoluteColonIndx := lengthStartPosition + lengthEndPosition
+	keyLength, err := strconv.Atoi(string(bytesArr[lengthStartPosition:absoluteColonIndx]))
 	if err != nil {
-		return 0, 0, fmt.Errorf("invalid string length, %s", err)
+		fmt.Print("Error with conversion to int")
+
+		return -1, -1, errors.New("Conversion failed")
 	}
-	return length, absoluteColonIdx, nil
+
+	return keyLength, absoluteColonIndx + 1, nil
+}
+
+func indexOf(haystack []byte, needle byte) int {
+	for i, v := range haystack {
+		if v == needle {
+			return i
+		}
+	}
+	return -1
 }
